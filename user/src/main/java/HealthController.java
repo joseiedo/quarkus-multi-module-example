b1a6e6868865fadb84e57c4302ad51c4b61ede57
iedo <iedoduarte1@gmail.com>
2025-11-15 15:47:34 -0300

    refactor: reorganize folder structure

---
+++ user/src/main/java/HealthController.java
@@ -0,0 +1,20 @@
+// import jakarta.ws.rs.GET;
+// import jakarta.ws.rs.Path;
+
+// @Path("/user")
+// public class HealthController2 {
+// 	//
+// 	// private UserService userService;
+// 	//
+// 	// public HealthController2(
+// 	// 	UserService userService
+// 	// ) {
+// 	// 	this.userService = userService;
+// 	// }
+// 	//
+// 	// // @GET
+// 	// // @Path("/health")
+// 	// public String health() {
+// 	// 	return userService.get();
+// 	// }
+// }
